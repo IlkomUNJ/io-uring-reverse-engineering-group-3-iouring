7a0d1e5e209140c0564e0244d09defbab304a13d
Zielknz <faturkautsarrahmann@gmail.com>
2025-05-13 11:56:30 +0700

    Update comments in futex.h

--- io_uring/futex.h
+++ io_uring/futex.h
@@ -16,18 +16,42 @@ bool io_futex_remove_all(struct io_ring_ctx *ctx, struct io_uring_task *tctx,
 bool io_futex_cache_init(struct io_ring_ctx *ctx);
 void io_futex_cache_free(struct io_ring_ctx *ctx);
 #else
+/*
+ * io_futex_cancel - TODO: Describe what this function does.
+ * @param struct io_ring_ctx *ctx
+ * @param struct io_cancel_data *cd
+ * @param unsigned int issue_flags
+ * @return TODO: Return value description.
+ */
 static inline int io_futex_cancel(struct io_ring_ctx *ctx,
 				  struct io_cancel_data *cd,
 				  unsigned int issue_flags)
 {
 	return 0;
+/*
+ * io_futex_remove_all - TODO: Describe what this function does.
+ * @param struct io_ring_ctx *ctx
+ * @param struct io_uring_task *tctx
+ * @param bool cancel_all
+ * @return TODO: Return value description.
+ */
 }
 static inline bool io_futex_remove_all(struct io_ring_ctx *ctx,
 				       struct io_uring_task *tctx, bool cancel_all)
 {
+/*
+ * io_futex_cache_init - TODO: Describe what this function does.
+ * @param struct io_ring_ctx *ctx
+ * @return TODO: Return value description.
+ */
 	return false;
 }
 static inline bool io_futex_cache_init(struct io_ring_ctx *ctx)
+/*
+ * io_futex_cache_free - TODO: Describe what this function does.
+ * @param struct io_ring_ctx *ctx
+ * @return TODO: Return value description.
+ */
 {
 	return false;
 }
