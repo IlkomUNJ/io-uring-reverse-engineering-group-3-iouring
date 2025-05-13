df0867324a8e8145bf5278f3741aa191dc036319
almahbubyanwar <almahbubyanwar@protonmail.com>
2025-05-13 18:05:24 +0700

    updated napi.h

--- io_uring/napi.h
+++ io_uring/napi.h
@@ -6,7 +6,10 @@
 #include <linux/kernel.h>
 #include <linux/io_uring.h>
 #include <net/busy_poll.h>
-
+/*
+    Implementation of the functions depend on whether CONFIG_NET_RX_BUSY_POLL
+    is enable in the kernel config.
+*/
 #ifdef CONFIG_NET_RX_BUSY_POLL
 
 void io_napi_init(struct io_ring_ctx *ctx);
@@ -20,22 +23,18 @@ int __io_napi_add_id(struct io_ring_ctx *ctx, unsigned int napi_id);
 void __io_napi_busy_loop(struct io_ring_ctx *ctx, struct io_wait_queue *iowq);
 int io_napi_sqpoll_busy_poll(struct io_ring_ctx *ctx);
 /*
- * io_napi - TODO: Describe what this function does.
- * @param struct io_ring_ctx *ctx
- * @return TODO: Return value description.
- */
-
+    When the flag is defined, checks if NAPI integration is currently
+    active and configured for the given io_uring context.
+    Returns true If NAPI tracking is active for this context
+    (i.e., NAPI instances are registered); false otherwise.
+*/
 static inline bool io_napi(struct io_ring_ctx *ctx)
 {
 	return !list_empty(&ctx->napi_list);
-/*
- * io_napi_busy_loop - TODO: Describe what this function does.
- * @param struct io_ring_ctx *ctx
- * @param struct io_wait_queue *iowq
- * @return TODO: Return value description.
- */
 }
-
+/*
+    Initiates a NAPI busy polling loop if NAPI is active for the context.
+*/
 static inline void io_napi_busy_loop(struct io_ring_ctx *ctx,
 				     struct io_wait_queue *iowq)
 {
@@ -43,18 +42,10 @@ static inline void io_napi_busy_loop(struct io_ring_ctx *ctx,
 		return;
 	__io_napi_busy_loop(ctx, iowq);
 }
-
 /*
- * io_napi_add() - Add napi id to the busy poll list
- * @req: pointer to io_kiocb request
- *
-/*
- * io_napi_add - TODO: Describe what this function does.
- * @param struct io_kiocb *req
- * @return TODO: Return value description.
- */
- * Add the napi id of the socket to the napi busy poll list and hash table.
- */
+    Dynamically adds the NAPI ID associated with a network socket
+    (from an I/O request) to the io_uring context's NAPI tracking list.
+*/
 static inline void io_napi_add(struct io_kiocb *req)
 {
 	struct io_ring_ctx *ctx = req->ctx;
@@ -66,59 +57,32 @@ static inline void io_napi_add(struct io_kiocb *req)
 	sock = sock_from_file(req->file);
 	if (sock && sock->sk)
 		__io_napi_add_id(ctx, READ_ONCE(sock->sk->sk_napi_id));
-/*
- * io_napi_init - TODO: Describe what this function does.
- * @param struct io_ring_ctx *ctx
- * @return TODO: Return value description.
- */
 }
 
 #else
-/*
- * io_napi_free - TODO: Describe what this function does.
- * @param struct io_ring_ctx *ctx
- * @return TODO: Return value description.
- */
-
+/* When the flag is disabled, these two functions do nothing. */
 static inline void io_napi_init(struct io_ring_ctx *ctx)
 {
 }
 static inline void io_napi_free(struct io_ring_ctx *ctx)
 {
-/*
- * io_unregister_napi - TODO: Describe what this function does.
- * @param struct io_ring_ctx *ctx
- * @param void __user *arg
- * @return TODO: Return value description.
- */
 }
+// When the flag is disabled, these two functions return -EOPNOTSUPP
+// (operation not supported).
 static inline int io_register_napi(struct io_ring_ctx *ctx, void __user *arg)
 {
-/*
- * io_napi - TODO: Describe what this function does.
- * @param struct io_ring_ctx *ctx
- * @return TODO: Return value description.
- */
 	return -EOPNOTSUPP;
 }
 static inline int io_unregister_napi(struct io_ring_ctx *ctx, void __user *arg)
-/*
- * io_napi_add - TODO: Describe what this function does.
- * @param struct io_kiocb *req
- * @return TODO: Return value description.
- */
 {
 	return -EOPNOTSUPP;
 }
+/* When the flag is disabled, this returns false */
 static inline bool io_napi(struct io_ring_ctx *ctx)
 {
 	return false;
-/*
- * io_napi_sqpoll_busy_poll - TODO: Describe what this function does.
- * @param struct io_ring_ctx *ctx
- * @return TODO: Return value description.
- */
 }
+/* When the flag is disabled, these two functions do nothing. */
 static inline void io_napi_add(struct io_kiocb *req)
 {
 }
@@ -126,6 +90,7 @@ static inline void io_napi_busy_loop(struct io_ring_ctx *ctx,
 				     struct io_wait_queue *iowq)
 {
 }
+/* When the flag is disabled, returns 0. */
 static inline int io_napi_sqpoll_busy_poll(struct io_ring_ctx *ctx)
 {
 	return 0;
