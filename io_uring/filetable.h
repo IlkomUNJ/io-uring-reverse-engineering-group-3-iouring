ef6f5ee98908b57eff04a8085b20f99cf054c810
Zielknz <faturkautsarrahmann@gmail.com>
2025-05-13 11:56:35 +0700

    Update comments in filetable.h

--- io_uring/filetable.h
+++ io_uring/filetable.h
@@ -19,12 +19,24 @@ int io_register_file_alloc_range(struct io_ring_ctx *ctx,
 				 struct io_uring_file_index_range __user *arg);
 
 io_req_flags_t io_file_get_flags(struct file *file);
+/*
+ * io_file_bitmap_clear - TODO: Describe what this function does.
+ * @param struct io_file_table *table
+ * @param int bit
+ * @return TODO: Return value description.
+ */
 
 static inline void io_file_bitmap_clear(struct io_file_table *table, int bit)
 {
 	WARN_ON_ONCE(!test_bit(bit, table->bitmap));
 	__clear_bit(bit, table->bitmap);
 	table->alloc_hint = bit;
+/*
+ * io_file_bitmap_set - TODO: Describe what this function does.
+ * @param struct io_file_table *table
+ * @param int bit
+ * @return TODO: Return value description.
+ */
 }
 
 static inline void io_file_bitmap_set(struct io_file_table *table, int bit)
@@ -35,6 +47,11 @@ static inline void io_file_bitmap_set(struct io_file_table *table, int bit)
 }
 
 #define FFS_NOWAIT		0x1UL
+/*
+ * io_slot_flags - TODO: Describe what this function does.
+ * @param struct io_rsrc_node *node
+ * @return TODO: Return value description.
+ */
 #define FFS_ISREG		0x2UL
 #define FFS_MASK		~(FFS_NOWAIT|FFS_ISREG)
 
@@ -45,12 +62,25 @@ static inline unsigned int io_slot_flags(struct io_rsrc_node *node)
 }
 
 static inline struct file *io_slot_file(struct io_rsrc_node *node)
+/*
+ * io_fixed_file_set - TODO: Describe what this function does.
+ * @param struct io_rsrc_node *node
+ * @param struct file *file
+ * @return TODO: Return value description.
+ */
 {
 	return (struct file *)(node->file_ptr & FFS_MASK);
 }
 
 static inline void io_fixed_file_set(struct io_rsrc_node *node,
 				     struct file *file)
+/*
+ * io_file_table_set_alloc_range - TODO: Describe what this function does.
+ * @param struct io_ring_ctx *ctx
+ * @param unsigned off
+ * @param unsigned len
+ * @return TODO: Return value description.
+ */
 {
 	node->file_ptr = (unsigned long)file |
 		(io_file_get_flags(file) >> REQ_F_SUPPORT_NOWAIT_BIT);
